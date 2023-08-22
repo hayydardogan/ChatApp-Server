ac182ad31b327b1dd2fc3884099b4163354d0596
Haydar Dogan <hayydardogan@users.noreply.github.com>
2023-08-22 14:39:17 +0300

    ChatApp-Server v2

--- public/style/js/chat.js
+++ public/style/js/chat.js
@@ -6,24 +6,15 @@ const submitBtn = document.getElementById('submitBtn');
 const output = document.getElementById('output');
 const feedback = document.getElementById('feedback');
 
-submitBtn.addEventListener('click', () => {
-    socket.emit('chat', {
-        message: message.value,
-        sender: sender.value
-    })
-})
 
-socket.on('chat', data => {
-    feedback.innerHTML = "";
-    output.innerHTML += '<p><strong>' + data.sender + ' : </strong>' + data.message + '</p>';
-    message.value = "";
 
-})
 
+
+
+// Ekleyebiliriz
 message.addEventListener('keypress', () => {
     socket.emit('typing', sender.value)
 })
-
 socket.on('typing', data => {
     feedback.innerHTML = '<p>' + data + ' yazıyor...' + '</p>';
 })
--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 const express = require('express')
 const socket = require('socket.io')
+const cors = require('cors');
+
 
 const app = express()
 const server = app.listen(8080)
+app.options("*", cors());
+app.use(cors());
 
 app.use(express.static('public'))
 
-const io = socket(server)
+let temp = "";
+
+const io = socket(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "OPTIONS"],
+    },
+});
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    // console.log(socket.id);
 
-    socket.on('chat', data => {
-        io.sockets.emit('chat', data)
+    socket.emit("welcome_message", "SocialMediaApp-Chat uygulamasına hoşgeldiniz...");
+
+    socket.on("send_message", async (data) => {
+        temp = '<p><strong>' + data.sender + ' : </strong>' + data.message + '</p>';
+        await io.sockets.emit('chat', temp)
     })
 
     socket.on('typing', data => {
         socket.broadcast.emit('typing', data)
     })
 })
+app.use(express.json());
